f5655b6b04adf698055c39e838cd2cae013d6337
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-09-05 22:11:15 +0200

    esercizio 2 finito

--- src/Esercizio_2/Corso.java
+++ src/Esercizio_2/Corso.java
@@ -2,4 +2,22 @@ package Esercizio_2;
 
 public class Corso {
 
+    String nomeCorso;
+
+    public Corso(String nomeCorso){
+        this.nomeCorso = nomeCorso;
+    }
+    
+    public void iscrivi(Studente studente){
+        System.out.println("Lo studente " + studente.getNome() + " " + studente.getCognome() + " è iscritto al corso di " + this.getNomeCorso() + " ." );
+    }
+
+    public String getNomeCorso() {
+        return nomeCorso;
+    }
+
+    public void setNomeCorso(String nomeCorso) {
+        this.nomeCorso = nomeCorso;
+    }
+
 }
--- src/Esercizio_2/Main.java
+++ src/Esercizio_2/Main.java
@@ -2,4 +2,15 @@ package Esercizio_2;
 
 public class Main {
 
+    public static void main(String[] args) {
+        
+        Studente s1 = new Studente();
+    
+        s1.setNome("Mario");
+        s1.setCognome("Rossi");
+
+        Corso c1 = new Corso("Matematica");
+
+        c1.iscrivi(s1);
+    }
 }
